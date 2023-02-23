fa51f45cce3739514cdebfa4ca89e7ed7c926a3e
detachhead <detachhead@users.noreply.github.com>
2023-02-23 10:48:00 +1000

    support `$ref`s

--- src/lib/SubSchemaForm.svelte
+++ src/lib/SubSchemaForm.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
-	import type { CommonComponentParameters } from "./types/CommonComponentParameters";
-	import { editorForSchema } from "./types/schema";
-	export let params: CommonComponentParameters;
-	export let schema: any;
-	export let value: any;
-	let { components } = params;
+  import type { CommonComponentParameters } from "./types/CommonComponentParameters";
+  import { editorForSchema } from "./types/schema";
+  import { resolveRefs } from "json-refs";
+  export let params: CommonComponentParameters;
+  export let schema: any;
+  export let value: any;
 
-	let typeComponent: any;
-	
-	let component: new (...args: any[]) => any;
-	$: component = components[editorForSchema(schema)];
+  let { components } = params;
+
+  let typeComponent: any;
+
+  const getComponent = (resolvedSchema: any) => components[editorForSchema(resolvedSchema)];
 </script>
 
-<svelte:component this={component} {params} {value} bind:schema />
-
-
-
+{#await resolveRefs(schema)}
+  <p>loading...</p>
+{:then result}
+  <svelte:component this={getComponent(result.resolved)} {params} {value} schema={result.resolved} />
+{/await}
